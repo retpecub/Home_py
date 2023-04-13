91dd56995d1ecb311e033fe512502f038f135b55
Колотев Руслан Витальевич <38437116+retpecub@users.noreply.github.com>
2023-04-13 21:32:37 +0300

    Update Home_work_task_2.py

--- Home_work_task_2.py
+++ Home_work_task_2.py
@@ -1,62 +1,26 @@
 month = input("Введите месяц: ")
 date = int(input("Введите число: "))
-if month == "март":
-    if date >= 21:
-        print("Овен")
-    else:
-        print('Рыбы')
-if month == "апрель":
-    if date >= 21:
-        print("Телец")
-    else:
-        print('Овен')
-if month == "май":
-    if date >= 22:
-        print("Близнецы")
-    else:
-        print('Телец')
-if month == "июнь":
-    if date >= 22:
-        print("Рак")
-    else:
-        print('Близнецы')
-if month == "июль":
-    if date >= 23:
-        print("Лев")
-    else:
-        print('Рак')
-if month == "август":
-    if date >= 24:
-        print("Дева")
-    else:
-        print('Лев')
-if month == "сентябрь":
-    if date >= 23:
-        print("Весы")
-    else:
-        print('Лев')
-if month == "октябрь":
-    if date >= 23:
-        print("Скорпион")
-    else:
-        print('Весы')
-if month == "ноябрь":
-    if date >= 22:
-        print("Стрелец")
-    else:
-        print('Скорпион')
-if month == "декабрь":
-    if date >= 22:
-        print("Козерог")
-    else:
-        print('Стрелец')
-if month == "январь":
-    if date >= 21:
-        print("Водолей")
-    else:
-        print('Козерог')
-if month == "февраль":
-    if date >= 20:
-        print("Рыбы")
-    else:
-        print('Водолей')
+if month == "март" and date >= 21 or month =="апрель" and date <= 20:
+    print("Овен")
+elif month == "апрель" and date >= 21 or month =="май" and date <= 21:
+    print("Телец")
+elif month == "май" and date >= 22 or month =="июнь" and date <= 21:
+    print("Близнецы")
+elif month == "июнь" and date >= 22 or month =="июль" and date <= 22:
+    print("Рак")
+elif month == "июль" and date >=23 or month =="август" and date <=21:
+    print("Лев")
+elif month == "август" and date >=22 or month =="сентябрь" and date <=23:
+    print("Дева")
+elif month == "сентябрь" and date >=24 or month =="октябрь" and date <=23:
+    print("Весы")
+elif month == "октябрь" and date >=24 or month =="ноябрь" and date <=22:
+    print("Скорпион")
+elif month == "ноябрь" and date >=23 or month =="декабрь" and date <=22:
+    print("Стрелец")
+elif month == "декабрь" and date >=23 or month =="январь" and date <=20:
+    print("Козерог")
+elif month == "январь" and date >=21 or month =="февраль" and date <=19:
+    print("Водолей")
+elif month == "февраль" and date >=20 or month =="март" and date <=20:
+    print("Рыбы")
